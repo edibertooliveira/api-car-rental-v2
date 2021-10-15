0ff2b4511506b48c29dbe6c74c7055a1b574ceb1
Ediberto Oliveira <edibertooliveira@aol.com>
2021-10-15 11:31:58 -0300

    feat: added DTO file controller from import categorie

---
+++ src/modules/categories/dtos/FileImportCategoryDTO.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class FileImportCategoryDTO {
+  @ApiProperty()
+  fieldname: string;
+  @ApiProperty()
+  originalname: string;
+  @ApiProperty()
+  encoding: string;
+  @ApiProperty()
+  mimetype: string;
+  @ApiProperty()
+  size: number;
+  @ApiProperty()
+  destination: string;
+  @ApiProperty()
+  filename: string;
+  @ApiProperty()
+  path: string;
+  @ApiProperty()
+  buffer: Buffer;
+}
--- src/modules/categories/infra/http/controllers/ImportCategories.controllers.ts
+++ src/modules/categories/infra/http/controllers/ImportCategories.controllers.ts
@@ -21,6 +21,7 @@ import {
 } from '@config/constants/response';
 import { summaryCreate } from '@config/constants/docs';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { FileImportCategoryDTO } from '@modules/categories/dtos/FileImportCategoryDTO';
 
 @ApiTags('Categories')
 @ApiBadRequestResponse({
@@ -39,7 +40,7 @@ export class CreateCarController {
   @HttpCode(HttpStatus.CREATED)
   @Post('imports')
   @UseInterceptors(FileInterceptor('file'))
-  handler(@UploadedFile() file) {
+  handler(@UploadedFile() file: FileImportCategoryDTO) {
     return { file };
   }
 }
