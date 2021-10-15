9c64b625953ecb5ea14de81a6030b2ce6c94136c
Ediberto Oliveira <edibertooliveira@aol.com>
2021-10-15 11:53:06 -0300

    chore: remove swagger docs DTO categories import

--- src/modules/categories/dtos/FileImportCategoryDTO.ts
+++ src/modules/categories/dtos/FileImportCategoryDTO.ts
@@ -1,30 +1,19 @@
-import { ApiProperty } from '@nestjs/swagger';
-
 export class FileImportCategoryDTO {
-  @ApiProperty()
   fieldname: string;
 
-  @ApiProperty()
   originalname: string;
 
-  @ApiProperty()
   encoding: string;
 
-  @ApiProperty()
   mimetype: string;
 
-  @ApiProperty()
   size: number;
 
-  @ApiProperty()
   destination: string;
 
-  @ApiProperty()
   filename: string;
 
-  @ApiProperty()
   path: string;
 
-  @ApiProperty()
   buffer: Buffer;
 }
