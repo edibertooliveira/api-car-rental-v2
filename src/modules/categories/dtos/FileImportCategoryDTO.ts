61ce96dbdbb77dcfb86983c87ed9aa4841aae1b4
Ediberto Oliveira <edibertooliveira@aol.com>
2021-10-15 11:50:09 -0300

    fix: added DTO file controller from import categorie

--- src/modules/categories/dtos/FileImportCategoryDTO.ts
+++ src/modules/categories/dtos/FileImportCategoryDTO.ts
@@ -3,20 +3,28 @@ import { ApiProperty } from '@nestjs/swagger';
 export class FileImportCategoryDTO {
   @ApiProperty()
   fieldname: string;
+
   @ApiProperty()
   originalname: string;
+
   @ApiProperty()
   encoding: string;
+
   @ApiProperty()
   mimetype: string;
+
   @ApiProperty()
   size: number;
+
   @ApiProperty()
   destination: string;
+
   @ApiProperty()
   filename: string;
+
   @ApiProperty()
   path: string;
+
   @ApiProperty()
   buffer: Buffer;
 }
